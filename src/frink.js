e76e656aa34aa44e362e1146379d7eb11cb92339
W.S. Hager <wshager@gmail.com>
2017-03-17 15:15:29 +0100

    added client-only layer

---
+++ src/frink.js
@@ -0,0 +1,9 @@
+export { elem, attr, text , cdata, comment, processingInstruction, qname } from "./vnode";
+
+export { appendChild, insertBefore, removeChild } from "./modify";
+
+export { docIter, nextNode, firstChild, nextSibling, parent, children, childrenByName } from "./access";
+
+export { fromL3, toL3 } from "./l3";
+
+export { render } from "./render";
