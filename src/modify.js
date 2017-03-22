6325e35ce79734f0d16c23f17e37e77742e17a19
W.S. Hager <wshager@gmail.com>
2017-03-22 12:22:42 +0100

    cleanup, always ensureRoot

--- src/modify.js
+++ src/modify.js
@@ -1,61 +1,28 @@
-import { Node, Value, Step, restoreNode } from './vnode';
+import { Node, Value, Step, restoreNode, ensureRoot } from './vnode';
 
-import { lastNode, parent } from './access';
-
-function assertPath(node) {
-	if (node.index < 0) return node;
-	var lastIndex = node.path.length - 1;
-	if (node.index > lastIndex) {
-		console.log("Node not in path");
-		var last = node.path[lastIndex];
-		let next = nextNode(node);
-		while (next) {
-			next = nextNode(next);
-			if (next.inode === node.inode) {
-				return next;
-			}
-		}
-	}
-	return node;
-}
-
-function assertNotInPath(child) {
-	var check = assertPath(child);
-	if(check) {
-		console.log("child exists");
-		// TODO clone child
-	}
-	return child;
-}
+import { firstChild, lastNode, parent } from './access';
 
 export function appendChild(node, child) {
-	// check if path to node is set
-	//node = assertPath(node);
+	node = ensureRoot(node);
 	let last = lastNode(node);
 	if(node.type == 9 && node.inode.size > 0) {
 		throw new Error("Document can only contain one child.");
 	}
 	let index = node.index;
 	// create shallow copy of path down to lastchild of node
-	//let path = last.path.slice(0, last.index + 1);
-	node = node.clone();
-	//node.path = path;
 	if (typeof child.inode === "function") {
 		child.inode(node);
 	} else {
-		// TODO FIXME check if child exists as-is
-		//child = assertNotInPath(child);
-		//child = child.clone();
+		// TODO make protective clone (of inode)
 	}
-	// overwrite parent in prevNode
-	//node.inode = restoreNode(child.parent,node.inode);
-	//if(node.index < 0) return node;
-	while (node.parent.type != 9) {
+
+	while (node.parent) {
 		child = node;
-		node = node.parent.clone();
+		node = node.parent;
 		node.inode = restoreNode(node.inode.set(child.name,child.inode),node.inode);
 	}
-	return node;
+	// this ensures immutability
+	return node.type == 9 ? firstChild(node) : node;
 }
 
 function insertBefore(node,elem){
@@ -71,29 +38,13 @@ function insertBefore(node,elem){
 }
 
 export function removeChild(node,child){
-	node = assertPath(node);
-	child = assertPath(child);
-	let index = node.index;
-	// shallow copy up to, but not including, child
-	let path = child.path.slice(0, child.index);
+	node = ensureRoot(node);
 	let inode = node.inode.removeValue(child.name,child.inode);
-	node = node.clone();
-	node.path = path;
-	// overwrite parent in prevNode
 	node.inode = restoreNode(inode,node.inode);
-	if(node.index < 0) return node;
-	node.parent = restoreNode(node.parent.set(node.name,node.inode),node.parent);
-	node.path[node.index] = node;
-	child = node;
-	while (node.inode._depth > 1) {
-		// overwrite parent in prevNode
-		node = parent(node).clone();
-		node.inode = child.parent;
-		node.path = path;
-		node.path[node.index] = node;
-		node.parent = restoreNode(node.parent.set(node.name,node.inode),node.parent);
-		if(node.parent._type == 9) break;
+	while (node.parent) {
 		child = node;
+		node = node.parent;
+		node.inode = restoreNode(node.inode.set(child.name,child.inode),node.inode);
 	}
-	return node.path[index];
+	return node.type == 9 ? firstChild(node) : node;
 }
