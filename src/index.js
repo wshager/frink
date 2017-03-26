691624bc66ffc27b1cf60addab8aedfef20bc4d0
W.S. Hager <wshager@gmail.com>
2017-03-26 12:00:50 +0200

    import required

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ export function parseString(str, cb) {
     return parser.parseString(str, cb);
 }
 
-export { elem, attr, text , cdata, comment, processingInstruction, qname } from "./vnode";
+export { e, a, t, d , cdata, comment, processingInstruction, qname } from "./vnode";
 
 export { appendChild, insertBefore, removeChild } from "./modify";
 
-export { iter, docIter, nextNode, firstChild, nextSibling, parent, children, childrenByName, stringify } from "./access";
+export { iter, docIter, nextNode, firstChild, nextSibling, parent, children, childrenByName, stringify, element, node, text, attribute, select, child } from "./access";
 
 export { fromL3, toL3 } from "./l3";
 
 export { fromJS, toJS, iter as iterJS, toL3 as jsToL3, fromL3 as jsFromL3 } from "./json";
+
+export { seq, isSeq, toSeq } from "./seq";
+
+export { forEach, filter, foldLeft, cat, get, transform, compose, into } from "./transducers";
