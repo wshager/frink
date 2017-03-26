2c04047cd583bf7b81aef21ac8f307f4c5badff7
W.S. Hager <wshager@gmail.com>
2017-03-26 12:04:25 +0200

    added required methods + iterator, removed get

--- src/seq.js
+++ src/seq.js
@@ -6,35 +6,68 @@ LazySeq.prototype.push = function (v) {
 	return this.concat(v);
 };
 
+// TODO create seq containing iterator, partially iterated
 // we need this for transducers, because LazySeq is immutable
 LazySeq.prototype["@@append"] = LazySeq.prototype.push;
 
+LazySeq.prototype.__is_Seq = true;
 
 LazySeq.prototype.concat = function (...v) {
 	return new LazySeq(this.iterable.concat(v));
 };
 
-LazySeq.prototype.get = function(index){
-	var i = 0;
-	var iterable = this.iterable;
-	var iter = isIterable(iterable) ? iterable[Symbol.iterator]() : {
-		next:function(){
-			return {value: iterable, done: true};
-		}
-	};
-	var next = iter.next();
-	this.iterable = [];
-	while (!next.done) {
-		var v = next.value;
-		this.iterable.push(v);
-		if(i===index) {
-			this.rest = iter;
-			return v;
-		}
-		next = iter.next();
-	}
-};
-
 LazySeq.prototype.toString = function(){
 	return "["+this.iterable+"]";
 };
+
+LazySeq.prototype.count = function(){
+	return this.iterable.length;
+};
+
+Object.defineProperty(LazySeq.prototype,"size",{
+	get:function(){
+		return this.count();
+	}
+});
+
+function SeqIterator(iterable) {
+    this.iter = iterable[Symbol.iterator]();
+}
+
+SeqIterator.prototype["@@append"] = LazySeq.prototype.push;
+
+SeqIterator.prototype["@@empty"] = function(){
+	return new LazySeq();
+};
+
+const DONE = {
+    done: true
+};
+
+SeqIterator.prototype.next = function () {
+    var v = this.iter.next();
+    if (v.done) return DONE;
+    return v;
+};
+
+SeqIterator.prototype[Symbol.iterator] = function () {
+    return this;
+};
+
+LazySeq.prototype[Symbol.iterator] = function () {
+    return new SeqIterator(this.iterable);
+};
+
+export function toSeq(a){
+	return new LazySeq(a);
+}
+
+export function seq(...a){
+	return new LazySeq(a);
+}
+
+export function isSeq(a){
+	return !!a && a.__is_Seq;
+}
+
+export const Seq = LazySeq;
