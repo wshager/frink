dce3eb3abb0b6b370cd373932320c0603d85c2c9
W.S. Hager <wshager@gmail.com>
2017-04-10 11:09:25 +0200

    export everything as single letter for now

--- src/vnode.js
+++ src/vnode.js
@@ -6,7 +6,7 @@ import { prettyXML } from "./pretty";
 
 import { forEach, into } from "./transducers";
 
-import { seq } from "./seq";
+import { seq, isSeq } from "./seq";
 
 export function Value(type, name, value, depth) {
 	this._type = type;
@@ -15,12 +15,18 @@ export function Value(type, name, value, depth) {
 	this._depth = depth;
 }
 
+Value.prototype.__is_Value = true;
+
 Value.prototype.get = function(){
 	return this._value;
 };
 
 Value.prototype[Symbol.iterator] = function*(){
-	yield this._value;
+	yield this;
+};
+
+Value.prototype.values = function(){
+	return this[Symbol.iterator]();
 };
 
 Value.prototype.count = function(){
@@ -42,11 +48,6 @@ export function VNode(inode,type,name,value,parent,indexInParent){
 	this.value = value;
 	this.parent = parent;
 	this.indexInParent = indexInParent;
-	Object.defineProperty(this,"children",{
-		get: () => {
-			return into(this.inode,forEach((c,i) => new VNode(c,c._type,c._name,c._value,this.inode)), seq());
-		}
-	});
 }
 
 VNode.prototype.__is_VNode = true;
@@ -56,12 +57,40 @@ VNode.prototype.toString = function(){
 	return root.inode.toString();
 };
 
+export function vnode(inode, parent, indexInParent){
+	return new VNode(inode, inode._type, inode._ns ? q(inode._ns.uri, inode._name) : inode._name, inode._value, parent, indexInParent);
+}
+
+export function VNodeIterator(iter, parent, f){
+	this.iter = iter;
+	this.parent = parent;
+	this.f = f;
+	this.index = -1;
+	this.__is_VNodeIterator = true;
+}
+
+const DONE = {
+    done: true
+};
+
+VNodeIterator.prototype.next = function () {
+    var v = this.iter.next();
+	this.index++;
+    if (v.done) return DONE;
+    return { value: this.f(v.value,this.parent,this.index) };
+};
+
+// TODO create iterator that yields a node seq
+// position() should overwrite get(), but the check should be name or indexInParent
 VNode.prototype[Symbol.iterator] = function(){
-	return this.inode[Symbol.iterator]();
+	return new VNodeIterator(this.inode.values(),this, vnode);
 };
 
 VNode.prototype.get = function(idx){
-	return this.inode.get(idx);
+	var val = this.inode.get(idx);
+	if(!val) return [];
+	val = val.constructor == Array ? val : [val];
+	return new VNodeIterator(val[Symbol.iterator](), this, vnode);
 };
 
 export function Step(inode,parent,indexInParent){
@@ -76,12 +105,13 @@ Step.prototype.toString = function(){
 	return "Step {depth:"+this._depth+", closes:"+this.parent.name+"}";
 };
 
-export function emptyINode(type, name, depth, attrs) {
+export function emptyINode(type, name, depth, attrs, ns) {
     var inode = type == 5 ? rrb.empty.beginMutation() : ohamt.make().beginMutation();
     inode._type = type;
     inode._name = name;
     inode._depth = depth;
     inode._attrs = attrs;
+	inode._ns = ns;
     return inode;
 }
 
@@ -89,6 +119,7 @@ export function restoreNode(next,node){
 	next._type = node._type;
 	next._name = node._name;
 	next._attrs = node._attrs;
+	next._ns = node._ns;
 	next._depth = node._depth;
 	return next;
 }
@@ -102,8 +133,10 @@ function elemToString(e){
 		return z += " "+k+"=\""+v+"\"";
 	};
 	let str = "<"+e._name;
+	let ns = e._ns;
+	if(ns) str += " xmlns" + (ns.prefix ? ":" + ns.prefix : "") + "=\"" + ns.uri + "\"";
 	str = e._attrs.reduce(attrFunc,str);
-	if(e._size){
+	if(e.size > 0){
 		str += ">";
 		for(let c of e.values()){
 			str += c.toString(false);
@@ -116,6 +149,8 @@ function elemToString(e){
 }
 
 var OrderedMap = ohamt.empty.constructor;
+
+OrderedMap.prototype.__is_Map = true;
 
 OrderedMap.prototype.toString = function(root = true, json = false){
 	var str = "";
@@ -142,6 +177,8 @@ OrderedMap.prototype.toString = function(root = true, json = false){
 
 var List = rrb.empty.constructor;
 
+List.prototype.__is_List = true;
+
 List.prototype.toString = function(root = true, json = false){
 	var str = "[";
 	for(var i=0,l = this.size; i < l; ){
@@ -152,9 +189,94 @@ List.prototype.toString = function(root = true, json = false){
 	return str + "]";
 };
 
-export function map(name,children){
-
+function _modify(pinode,node,ref) {
+	var type = pinode._type;
+	if(type == 1){
+		if (ref !== undefined) {
+			return restoreNode(pinode.insertBefore([ref.name,ref.inode],[node.name,node.inode]), pinode);
+		} else {
+			// FIXME check the parent type
+			return restoreNode(pinode.push([node.name,node.inode]), pinode);
+		}
+	} else if(type == 5){
+		if (ref !== undefined) {
+			return restoreNode(pinode.insertBefore(ref,node.inode), pinode);
+		} else {
+			return restoreNode(pinode.push(node.inode), pinode);
+		}
+	} else if(type == 6){
+		return restoreNode(pinode.set(node.name,node.inode), pinode);
+	}
 }
+
+function _n(type, name, children){
+	if(isSeq(children)) children = children.toArray();
+	if(children.constructor != Array) {
+		if(children === undefined) {
+			children = [];
+		} else {
+			if(!children.__is_VNode) children = x(children);
+			children = [children];
+		}
+	}
+	var node = new VNode(function (parent, ref) {
+		let pinode = parent.inode;
+		let name = node.name, ns;
+		if(type == 1) {
+			if(_isQName(name)) {
+				ns = name;
+				name = name.name;
+			} else if(/:/.test(name)){
+				// TODO where are the namespaces?
+			}
+		}
+		let inode = emptyINode(type, name, pinode._depth + 1, type == 1 ? ohamt.empty.beginMutation() : undefined, ns);
+		node.inode = inode;
+		for (let i = 0; i < children.length; i++) {
+			let child = children[i];
+			child = child.inode(node);
+		}
+		node.inode = node.inode.endMutation();
+		// insert into the parent means: update all parents until we come to the root
+		// but the parents of my parent will be updated elsewhere
+		// we just mutate the parent, because it was either cloned or newly created
+		parent.inode = _modify(pinode, node, ref);
+		node.parent = parent;
+		return node;
+	}, type, name);
+	return node;
+}
+
+function _a(type, name, value) {
+	var node = new VNode(function (parent, ref) {
+		let attrMap = parent.inode._attrs;
+		if (ref !== undefined) {
+			parent.inode._attrs = attrMap.insertBefore([ref.name,ref.value],[name,value]);
+		} else {
+			parent.inode._attrs = attrMap.push([name,value]);
+		}
+		node.parent = parent;
+		return node;
+	}, type, name, value);
+	return node;
+}
+
+function _v(type,value,name) {
+	var node = new VNode(function (parent, ref) {
+		let pinode = parent.inode;
+		// reuse insertIndex here to create a named map entry
+		if(node.name === undefined) node.name = pinode.count() + 1;
+		node.inode = new Value(node.type, node.name, value, pinode._depth + 1);
+		node.name = name;
+		// we don't want to do checks here
+		// we just need to call a function that will insert the node into the parent
+		parent.inode = _modify(pinode,node,ref);
+		node.parent = parent;
+		return node;
+	}, type, name, value);
+	return node;
+}
+
 /**
  * Create a provisional element VNode.
  * Once the VNode's inode function is called, the node is inserted into the parent at the specified index
@@ -162,77 +284,59 @@ export function map(name,children){
  * @param  {[type]} children [description]
  * @return {[type]}          [description]
  */
-export function e(name, children) {
-	var node = new VNode(function (parent, ref) {
-		var attrMap = ohamt.empty.beginMutation();
-		let pinode = parent.inode;
-		let inode = emptyINode(1, name, pinode._depth + 1, attrMap);
-		node.inode = inode;
-		for (let i = 0; i < children.length; i++) {
-			let child = children[i];
-			if (child.type == 2) {
-				attrMap = attrMap.set(child.name, child.value);
-			} else {
-				// this call will mutate me (node)!
-				child = child.inode(node);
-				//node.inode = restoreNode(child.parent, node.inode);
-			}
-		}
-		node.inode = node.inode.endMutation();
-		node.inode._attrs = attrMap.endMutation(true);
-		// insert into the parent means: update all parents until we come to the root
-		// but the parents of my parent will be updated elsewhere
-		// we just mutate the parent, because it was either cloned or newly created
-		if (ref !== undefined) {
-			parent.inode = restoreNode(pinode.insertBefore([ref.name,ref.inode],[node.name,node.inode]), pinode);
-		} else {
-			// FIXME check the parent type
-			parent.inode = restoreNode(pinode.push([node.name,node.inode]), pinode);
-		}
-		node.parent = parent;
-		return node;
-	}, 1, name);
-	return node;
+export function e(qname, children) {
+	return _n(1,qname,children);
 }
 
-export function a(name, value) {
-	var node = new VNode(function (parent, ref) {
-		let attrMap = parent._attrs;
-		if (ref !== undefined) {
-			parent._attrs = attrMap.insertBefore([ref.name,ref.value],[name,value]);
-		} else {
-			parent._attrs = attrMap.push([name,value]);
-		}
-		node.parent = parent;
-		return node;
-	}, 2, name, value);
-	return node;
+export function l(name, children) {
+	if(arguments.length == 1) {
+		children = name;
+		name = "#";
+	}
+	return _n(5,name,children);
 }
 
-export function t(value) {
-	var node = new VNode(function (parent, insertIndex = -1) {
-		let pinode = parent.inode;
-		// reuse insertIndex here to create a named map entry
-		let name = insertIndex > -1 ? insertIndex : pinode.count() + 1;
-		node.name = name;
-		node.inode = new Value(3, name, value, pinode._depth + 1);
-		// we don't want to do checks here
-		// we just need to call a function that will insert the node into the parent
-		parent.inode = restoreNode(pinode.push([name,node.inode]),pinode);
-		node.parent = parent;
-		return node;
-	}, 3, null, value);
-	return node;
+export function m(name,children){
+	if(arguments.length == 1) {
+		children = name;
+		name = "#";
+	}
+	return _n(6,name,children);
 }
 
-export function d() {
-	return new VNode(emptyINode(9,"#document",0,ohamt.empty), 9, "#document");
+export function a(name,value){
+	return _a(2,name,value);
+}
+
+export function p(name,value){
+	return _a(7,name,value);
+}
+
+export function x(name, value) {
+	if(arguments.length == 1) value = name;
+	return _v(typeof value == "string" ? 3 : 12, value, name);
+}
+
+export function c(value, name){
+	return _v(8, value, name);
+}
+
+export function d(uri = null,prefix = null,doctype = null) {
+	var attrs = ohamt.empty;
+	if(uri) {
+		attrs = attrs.set("xmlns" + (prefix ? ":" + prefix : ""), uri);
+	}
+	if(doctype) {
+		attrs = attrs.set("DOCTYPE", doctype);
+	}
+	return new VNode(emptyINode(9,"#document",0, attrs), 9, "#document");
 }
 
 export function ensureRoot(node){
+	if(!node) return;
 	if(!node.inode) {
 		let root = node.first();
-		return new VNode(root, root._type, root._name, root._value, new VNode(node,node._type,node._name), 0);
+		return vnode(root, vnode(node));
 	}
 	if(typeof node.inode === "function") {
 		node.inode(d());
@@ -246,9 +350,13 @@ export function _isQName(maybe){
 }
 
 export function QName(uri, name) {
+	var prefix = /:/.test(name) ? name.replace(/:.+$/,"") : null;
     return {
         __is_QName: true,
 		name: name,
+		prefix,
         uri: uri
     };
 }
+
+export const q = QName;
