e1869efa92a932d01445ef94248fae7f8c7953e5
Wouter Hager <wshager@gmail.com>
2017-04-10 18:11:27 +0200

    Update construct.js

--- test/construct.js
+++ test/construct.js
@@ -1,7 +1,4 @@
-const n = require("../lib/xvptree");
+const n = require("../lib/index");
 
-var e = n.elem("test",[n.text("bla")]);
-var d = n.appendChild(n.doc(),e);
-console.log(d)
-var x = n.appendChild(n.nextNode(d),e);
-console.log(x.toString());
+var e = n.e("test",[n.t("bla")]);
+console.log(e.toString());
