cee01fc8e4cdfef5366bba428387e148eea28939
W.S. Hager <wshager@gmail.com>
2017-04-10 11:10:58 +0200

    added operations

--- src/seq.js
+++ src/seq.js
@@ -1,5 +1,5 @@
 export function LazySeq(iterable){
-	this.iterable = iterable || [];
+	this.iterable = isSeq(iterable) ? iterable.toArray() : iterable || [];
 }
 
 LazySeq.prototype.push = function (v) {
@@ -22,6 +22,10 @@ LazySeq.prototype.toString = function(){
 
 LazySeq.prototype.count = function(){
 	return this.iterable.length;
+};
+
+LazySeq.prototype.toArray = function() {
+	return this.iterable;
 };
 
 Object.defineProperty(LazySeq.prototype,"size",{
@@ -58,12 +62,24 @@ LazySeq.prototype[Symbol.iterator] = function () {
     return new SeqIterator(this.iterable);
 };
 
-export function toSeq(a){
-	return new LazySeq(a);
+function _isArray(a){
+	return !!a && a.constructor == Array;
 }
 
 export function seq(...a){
-	return new LazySeq(a);
+	var ret = [];
+	for(var i = 0, l = a.length; i < l; i++){
+		var x = a[i];
+		if(_isArray(x)) {
+			//  assume flat
+			ret = ret.concat(x);
+		} else if(isSeq(x)){
+			ret = ret.concat(x.toArray());
+		} else {
+			ret.push(x);
+		}
+	}
+	return new LazySeq(ret);
 }
 
 export function isSeq(a){
@@ -71,3 +87,34 @@ export function isSeq(a){
 }
 
 export const Seq = LazySeq;
+
+export const isEmpty = s => isSeq(s) && !s.count();
+
+export const first = s => isSeq(s) ? _isArray(s.iterable) ? s.iterable[0] : _first(s.iterable) : s;
+
+const undef = s => s === undefined || s === null;
+
+export function empty(s){
+    return isSeq(s) ? !s.count() : undef(s);
+}
+
+export function exists($i){
+    return isSeq(s) ? !!s.count() : !undef(s);
+}
+
+export function count(s){
+	return empty(s) ? 0 : isSeq(s) ? s.count() : undef(s) ? 0 : 1;
+}
+
+export function insertBefore(s,pos,ins) {
+    pos = first(pos);
+    pos = pos === 0 ? 1 : pos - 1;
+	var a = s.toArray();
+	var n = a.slice(0,pos);
+	if(isSeq(ins)) {
+		n = n.concat(ins.toArray());
+	} else {
+		n.push(ins);
+	}
+    return seq(n.concat(a.slice(pos)));
+}
