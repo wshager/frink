c2d81140317f280a9cd80a9271a40b5ae874ea0d
wshager <wshager@gmail.com>
2017-04-11 20:29:03 +0200

    fixed array

--- src/array.js
+++ src/array.js
@@ -11,13 +11,25 @@ var List = rrb.empty.constructor;
 
 List.prototype.__is_List = true;
 
+List.prototype["@@empty"] = function(){
+	return rrb.empty;
+}
+
+List.prototype["@@append"] = List.prototype.push;
+
+rrb.TreeIterator.prototype["@@empty"] = function(){
+	return rrb.empty;
+}
+
+rrb.TreeIterator.prototype["@@append"] = List.prototype.push;
+
 
 export function isArray($maybe) {
 	let maybe = first($maybe);
 	return !!(maybe && maybe.__is_List);
 }
 
-export function array(...a) {
+export default function(...a) {
 	var l = a.length;
 	if(l===0){
 		return rrb.empty;
@@ -31,11 +43,11 @@ export function array(...a) {
 export function join($a) {
 	if ($a === undefined) return error("XPTY0004");
 	// assume a sequence of vectors
-	return foldLeft(a,function(pre,cur){
+	return foldLeft(a,rrb.empty,function(pre,cur){
 		var v = first(cur);
 		if(!isArray(v)) return error("XPTY0004","One of the items for array:join is not an array.");
 		return pre.concat(v);
-	},rrb.empty);
+	});
 }
 
 
@@ -47,6 +59,7 @@ function _checked($a, fn, ...args) {
 		a = first($a);
 	}
 	if (!isArray(a)) return error("XPTY0004");
+	args.unshift(a);
 	return fn.apply(a, args);
 }
 
@@ -69,13 +82,14 @@ export function subarray($a,$s,$e) {
 
 export function insertBefore($a, $i, $v) {
 	var i = first($i) || 1;
+	var ix = i.valueOf() - 1;
 	// unmarshal Singleton
 	var v = isSeq($v) && $v.size > 1 ? $v : first($v);
-	// slice from 0 to i
-	// slice form i to end
-	// concat s1 + v + s2
 	return _checked($a, function(a){
-		return a.slice(0,i).push(v).concat(a.slice(i));
+		// slice from 0 to i
+		// slice form i to end
+		// concat s1 + v + s2
+		return a.slice(0,ix).push(v).concat(a.slice(ix));
 	});
 }
 
@@ -83,7 +97,7 @@ export function remove($a,$i) {
 	var i = first($i) || 1;
 	var ix = i.valueOf() - 1;
 	return _checked($a, function(a){
-		return a.slice(0,ix).concat(a.slice(ix));
+		return a.slice(0,ix).concat(a.slice(i));
 	});
 }
 
