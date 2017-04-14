5dd1543eeb87fbfff179e39feb2726fc0d7910c2
wshager <wshager@gmail.com>
2017-04-14 18:50:01 +0200

    build+fixes

--- test/string.js
+++ test/string.js
@@ -6,6 +6,6 @@ const a = require("../lib/access");
 function assertEq(a,b){
 	assert.equal(JSON.stringify(a),JSON.stringify(b));
 }
-var x = n.analyzeString("add(1,1)","(\\)[\\+\\*\\-\\?]?)|(=#\\p{N}+#?\\p{N}*=|,)?([\\+\\*\\-\\?\\p{L}\\p{N}\\-_\\.@\\$%/#@\\^:]*)(\\(?)");
+var x = n.analyzeString("1+1","(\\)[\\+\\*\\-\\?]?)|(=#\\p{N}+#?\\p{N}*=|,)?([\\+\\*\\-\\?\\p{L}\\p{N}\\-_\\.@\\$%/#@\\^:]*)(\\(?)");
 var ret = n.select(x,"fn:match","fn:group",n.filter(_ => n.eq(n.select(_,"@nr"),3)));
-console.log(ret.toString());
+console.log(x.toString());
