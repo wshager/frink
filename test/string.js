e6cf3c635cceaf2a4824e222c5858c9994c6822c
wshager <wshager@gmail.com>
2017-04-14 14:15:11 +0200

    fixed regular expression

--- test/string.js
+++ test/string.js
@@ -1,11 +1,12 @@
 const n = require("../lib/string");
 const assert = require("assert");
 const t = require("../lib/transducers");
+const a = require("../lib/access");
+const fn = require("../lib/type");
 
 function assertEq(a,b){
 	assert.equal(JSON.stringify(a),JSON.stringify(b));
 }
-
-var x = n.analyzeString("+",/(\)[\+\*\-\?]?)|(=#\p{N}+#?\p{N}*=|,)?([\+\*\-\?\p{L}\p{N}\-_\.@\$%/#@\^:]*)(\(?)/);
-
-console.log(x.toString())
+var r = "(\\)[\\+\\*\\-\\?]?)|(=#\\p{N}+#?\\p{N}*=|,)?([\\+\\*\\-\\?\\p{L}\\p{N}\\-_\\.@\\$%/#@\\^:]*)(\\(?)";
+var x = n.analyzeString("add(1,1)",r);
+console.log(a.select(x,"fn:match",fn.string))
