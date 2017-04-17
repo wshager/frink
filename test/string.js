0ec10df1a2747099c83d5bdcaef41524938c3357
W.S. Hager <wshager@gmail.com>
2017-04-17 21:47:49 +0200

    head/tail test

--- test/string.js
+++ test/string.js
@@ -1,11 +1,13 @@
 const n = require("../lib/index");
 const assert = require("assert");
-const t = require("../lib/transducers");
 const a = require("../lib/access");
 
 function assertEq(a,b){
 	assert.equal(JSON.stringify(a),JSON.stringify(b));
 }
-var x = n.analyzeString("1+1","(\\)[\\+\\*\\-\\?]?)|(=#\\p{N}+#?\\p{N}*=|,)?([\\+\\*\\-\\?\\p{L}\\p{N}\\-_\\.@\\$%/#@\\^:]*)(\\(?)");
+var x = n.analyzeString("1 + 1","(\\)[\\+\\*\\-\\?]?)|(=#\\p{N}+#?\\p{N}*=|,)?([\\+\\*\\-\\?\\p{L}\\p{N}\\-_\\.@\\$%/#@\\^:]*)(\\(?)");
 var ret = n.select(x,"*");
 console.log(ret.toString());
+//var h = n.head(ret);
+var t = n.tail(ret);
+console.log(t.toString())
