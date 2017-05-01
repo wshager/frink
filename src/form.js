456bf0ea237d415d767725ce1b300e59a0f4755f
wshager <wshager@gmail.com>
2017-05-01 22:09:33 +0200

    fixed validation

--- src/form.js
+++ src/form.js
@@ -1,5 +1,7 @@
 import { VNode } from "./vnode";
 
+import { forEach } from "./transducers";
+
 function _inferType(node){
 	var t = node.dataType;
 	if(t) {
@@ -24,13 +26,45 @@ export function ivalue(type,name,value){
 }
 
 export function vnode(inode, parent, depth, indexInParent){
-	return new VNode(inode, _inferType(inode), inode.name, inode.value, parent, depth, indexInParent);
+	var type = _inferType(inode);
+	var format = inode.type;
+	var val = inode.value;
+	if(type == 12 && typeof val == "string"){
+		if(format == "checkbox") {
+			val = inode.checked;
+		} else if(format == "number"){
+			val = parseFloat(inode.value);
+		}
+	}
+	return new VNode(inode, type, inode.name, val, parent, depth, indexInParent);
 }
 
 export function next(inode, node, type){
-	type = type || _inferType(type);
+	//type = type || _inferType(type);
 	var idx = node.indexInParent;
 	// FIXME detect fieldset elements
 	var elems = inode.elements;
-	if(elems) return elems[idx++];
+	if(elems) return elems[idx+1];
+}
+
+export function count(inode,type){
+	return inode.elements ? inode.elements.length : 0;
+}
+
+export function keys(inode,type){
+	// TODO cached
+	return inode.elements ? forEach(inode.elements, _ => _.name) : [];
+}
+
+export function cached(){
+
+}
+
+export function first(inode,type){
+	// FIXME detect / filter fieldset elements
+	if(inode.elements) return inode.elements[0];
+}
+
+export function get(inode,idx,type){
+	return inode[idx];
 }
