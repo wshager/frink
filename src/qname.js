5cef1116a1a5d0efe408e422066a4c88d6ca106a
wshager <wshager@gmail.com>
2017-05-02 17:06:00 +0200

    moved qname to qname

---
+++ src/qname.js
@@ -0,0 +1,15 @@
+export function isQName(maybe){
+	return !!(maybe && maybe.__is_QName);
+}
+
+export function QName(uri, name) {
+	var prefix = /:/.test(name) ? name.replace(/:.+$/,"") : null;
+    return {
+        __is_QName: true,
+		name: name,
+		prefix,
+        uri: uri
+    };
+}
+
+export const q = QName;
