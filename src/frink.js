d4d1c6aba778f2039a1c7b7f5619debd9d85931e
whager <whager@benjamins.nl>
2017-05-03 12:53:50 +0200

    cleanup

--- src/frink.js
+++ src/frink.js
@@ -1,15 +1,13 @@
-export { e, a, x, l, m, p, q } from "./construct";
+export * from "./construct";
 
-export { appendChild, insertChildBefore, removeChild } from "./modify";
+export * from "./modify";
 
-export { docIter, nextNode, firstChild, nextSibling, parent, children, childrenByName } from "./access";
+export * from "./access";
 
-export { fromL3, toL3 } from "./l3";
+export * from "./l3";
 
 export * from "./validate";
 
-export { render } from "./render";
+export * from "./render";
 
 export * from "./dom-util";
-
-export * from "./form";
