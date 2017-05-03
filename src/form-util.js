8ed2dde9f6dfe992331ff4b9bc49a1bcb7d3e8b2
whager <whager@benjamins.nl>
2017-05-03 12:55:33 +0200

    added form-util + form layer

---
+++ src/form-util.js
@@ -0,0 +1,14 @@
+import { iter } from "./access";
+
+// iter form and replace fieldset types
+export function process(node){
+	iter.bind(this)(node,function(node){
+		if(node.type == 6){
+			// this is mutative
+			if (node.inode.dataset.appearance == "hidden") {
+				node.inode.disabled = true;
+				node.inode.hidden = true;
+			}
+		}
+	});
+}
---
+++ src/frinkiform.js
@@ -0,0 +1,27 @@
+import * as inode from "./iform";
+
+import * as dc from "./doc";
+
+import * as ac from "./access";
+
+import * as va from "./validate";
+
+import * as fu from "./form-util";
+
+export const ensureDoc = dc.ensureDoc.bind(inode);
+
+export const d = dc.d.bind(inode);
+
+export const firstChild = ac.firstChild.bind(inode);
+
+export const lastChild = ac.lastChild.bind(inode);
+
+export const select = ac.select.bind(inode);
+
+export const validate = va.validate.bind(inode);
+
+export const process = fu.process.bind(inode);
+
+export * from "./transducers";
+
+export * from "./dom-util";
