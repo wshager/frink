9c09a2107b8b5bc7cf970f89e0e2c59a42afbed2
whager <whager@benjamins.nl>
2017-05-03 12:53:35 +0200

    use cx in _ascend

--- src/modify.js
+++ src/modify.js
@@ -1,8 +1,8 @@
 import { ensureDoc } from "./doc";
 
-import { firstChild, lastChild } from './access';
+import { firstChild } from './access';
 
-function _ascend(node){
+function _ascend(node,cx){
 	var child;
 	while (node.parent) {
 		child = node;
@@ -10,24 +10,21 @@ function _ascend(node){
 		node = node.set(child.name,child.inode);
 	}
 	// this ensures immutability
-	return node.type == 9 ? firstChild(node) : node;
+	return node.type == 9 ? firstChild.bind(cx)(node) : node;
 }
 
 export function appendChild(node, child) {
 	node = ensureDoc.bind(this)(node);
 	//if(!node || !node.size) return;
-	//let last = lastChild(node);
 	if(node.type == 9 && node.inode.size > 0) {
 		throw new Error("Document can only contain one child.");
 	}
-	// create shallow copy of path down to lastchild of node
 	if (typeof child.inode === "function") {
 		child.inode(node);
 	} else {
-		// TODO make protective clone (of inode)
 		node = node.push(child);
 	}
-	return _ascend(node);
+	return _ascend(node,this);
 }
 
 export function insertChildBefore(node,ins){
@@ -40,7 +37,7 @@ export function insertChildBefore(node,ins){
 		// what?
 	}
 	node = parent;
-	return _ascend(node);
+	return _ascend(node,this);
 }
 
 export function removeChild(node,child){
@@ -49,5 +46,5 @@ export function removeChild(node,child){
 	// TODO error
 	if(child.parent.inode !== node.inode) return;
 	node = node.removeChild(child);
-	return _ascend(node);
+	return _ascend(node,this);
 }
