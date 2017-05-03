f4d5e0ec7d4d49e819cde2a35e48a05f44f58e16
whager <whager@benjamins.nl>
2017-05-03 12:54:53 +0200

    moved out parser-dependent stuff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import * as inode from "./inode";
 
+import { first } from "./seq";
+
 import { Parser } from "./parser";
+
+// TODO move to better location
+// TODO update when loader spec is solid
+// TODO add easy access to a xhr / db module
+import { readFileSync, readdirSync } from 'fs';
 
 export function parseString(str, cb) {
     var parser = new Parser(inode);
     return parser.parseString(str, cb);
+}
+
+export function parse($a){
+	var xml = first($a);
+    var result;
+    parseString(xml,function(err,ret){
+        if(err) console.log(err);
+        result = ret;
+    });
+    return result;
+}
+
+export function doc($file){
+    var file = first($file);
+    return parse(readFileSync(file.toString(),"utf-8"));
+}
+
+export function collection($uri) {
+    var uri = first($uri);
+    return seq(readDirSync(uri).map(file => doc(uri+"/"+file)));
 }
 
 export * from "./doc";
