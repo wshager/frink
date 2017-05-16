9f5bdaee1223f97cd450d8032ea81987b58216fd
wshager <wshager@gmail.com>
2017-05-16 12:43:15 +0200

    handle nodes without doc root correctly

--- src/doc.js
+++ src/doc.js
@@ -2,13 +2,16 @@ import * as inode from "./inode";
 
 export function ensureDoc(node){
 	if(!node) return;
-	var cx = this.vnode ? this : inode;
+	var cx = this && this.vnode ? this : inode;
 	if(!node.inode) {
 		if(cx.getType(node) == 9) {
 			let root = cx.first(node);
 			return cx.vnode(root, cx.vnode(node), 1, 0);
 		} else {
-			return cx.vnode(node, d(), 1, 0);
+			let doc = d.bind(cx)();
+			let root = cx.vnode(node, doc, 1, 0);
+			doc = doc.push(root);
+			return root;
 		}
 	}
 	if(typeof node.inode === "function") {
@@ -27,5 +30,5 @@ export function d(uri = null,prefix = null,doctype = null) {
 	if(doctype) {
 		attrs.DOCTYPE = doctype;
 	}
-	return cx.vnode(cx.emptyINode(9,"#document",0, cx.emptyAttrMap(attrs)), 9, "#document");
+	return cx.vnode(cx.emptyINode(9,"#document",0, cx.emptyAttrMap(attrs)));
 }
