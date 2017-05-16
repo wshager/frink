ee060f53399b8f337b119f7b35889a8b0594cf40
wshager <wshager@gmail.com>
2017-05-16 12:41:21 +0200

    added entries+values to object-util

--- src/object-util.js
+++ src/object-util.js
@@ -8,3 +8,13 @@ export const entries = function *(obj) {
         yield [key, obj[key]];
     }
 };
+
+export const values = function *(obj) {
+
+    const keys = Object.keys(obj);
+
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];
+        yield obj[key];
+    }
+};
--- src/values.js
+++
@@ -1,9 +0,0 @@
-export const values = function *(obj) {
-
-    const keys = Object.keys(obj);
-
-    for (let i = 0; i < keys.length; i++) {
-
-        yield obj[key];
-    }
-};
