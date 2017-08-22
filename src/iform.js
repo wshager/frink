bce3289ea041cb889c6058b5ea3e1d15c62665c9
wshager <wshager@gmail.com>
2017-08-22 08:45:02 +0200

    skip fieldsets

--- src/iform.js
+++ src/iform.js
@@ -8,14 +8,14 @@ function _inferType(node){
 	var t = node.dataType;
 	if(t) {
 		switch(t){
-			case "string":
+		case "string":
 			return 3;
-			case "boolean":
-			case "number":
+		case "boolean":
+		case "number":
 			return 12;
-			case "array":
+		case "array":
 			return 5;
-			case "object":
+		case "object":
 			return 6;
 		}
 	}
@@ -42,19 +42,29 @@ export function vnode(inode, parent, depth, indexInParent){
 	return new VNode(cx, inode, type, inode.name, val, parent, depth, indexInParent);
 }
 
-export function next(inode, node, type){
-	//type = type || _inferType(type);
-	var idx = node.indexInParent;
-	// FIXME detect fieldset elements
-	var elems = inode.elements;
-	if(elems) return elems[idx+1];
+function _inFieldset(node,parent){
+	while(node = node.parentNode, !!node && node != parent){
+		if(node.type == "fieldset") {
+			return true;
+		}
+	}
 }
 
-export function count(inode,type){
-	return inode.elements ? inode.elements.length : 0;
+export function count(inode){
+	// filter out elements that are in form, but also in fieldset...
+	if(!inode.elements) return 0;
+	var len = inode.elements.length;
+	if(inode.nodeName.toUpperCase() == "FORM"){
+		var c = 0;
+		for(var i = 0; i < len; i++){
+			if(!_inFieldset(inode.elements[i],inode)) c++;
+		}
+		return c;
+	}
+	return len;
 }
 
-export function keys(inode,type){
+export function keys(inode){
 	// TODO cached
 	return inode.elements ? forEach(inode.elements, _ => _.name) : [];
 }
@@ -63,11 +73,56 @@ export function cached(){
 
 }
 
-export function first(inode,type){
-	// FIXME detect / filter fieldset elements
-	if(inode.elements) return inode.elements[0];
+export function first(inode){
+	// detect / filter fieldset elements
+	if (inode.elements) {
+		var idx = 0;
+		var first = inode.elements[idx];
+		if(inode.nodeName.toUpperCase() == "FORM") {
+			while(first && _inFieldset(first,inode)) {
+				first = inode.elements[++idx];
+			}
+		}
+		return first;
+	}
 }
 
-export function get(inode,idx,type){
+export function next(inode, node){
+	//type = type || _inferType(type);
+	var idx = node.indexInParent;
+	// detect fieldset elements
+	var elems = inode.elements;
+	if (elems) {
+		var next = elems[idx + 1];
+		if(node.parent.inode.nodeName.toUpperCase() == "FORM") {
+			while(next && _inFieldset(next,node.parent.inode)) {
+				next = elems[++idx];
+			}
+		}
+		return next;
+	}
+}
+
+export function get(inode,idx){
 	return inode[idx];
+}
+
+export function getType(inode){
+	// probably only used for empty root
+	return 9;
+}
+
+export function emptyINode(type,name,depth,attrs){
+}
+
+export function emptyAttrMap(){
+	// probably only used for empty root
+}
+
+export function push() {
+
+}
+
+export function getAttribute(inode){
+	return inode.attributes[inode];
 }
