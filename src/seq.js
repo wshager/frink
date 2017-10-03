8da55c0a906d1a8ed3aea46415661ae7c633d075
wshager <wshager@gmail.com>
2017-10-03 15:23:08 +0200

    moved transduceObservable to transducers

--- src/seq.js
+++ src/seq.js
@@ -1,51 +1,10 @@
 import { Observable } from "rxjs/Observable";
-import { seq as transform } from "transducers.js";
+import { transform, isObservable } from "./transducers";
 import { error } from "./error";
 import "rxjs/add/observable/from";
 import "rxjs/add/operator/reduce";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/filter";
-
-
-function TransduceObserver(o, xform) {
-	this._o = o;
-	this._xform = xform;
-}
-
-TransduceObserver.prototype.next = function (x) {
-	this._xform["@@transducer/step"].call(this._xform, this._o, x);
-};
-
-TransduceObserver.prototype.error = function (e) { this._o.error(e); };
-
-TransduceObserver.prototype.completed = function () {
-	this._xform["@@transducer/result"](this._o);
-};
-
-function transformForObserver(o) {
-	return {
-		"@@transducer/init": function() {
-			return o;
-		},
-		"@@transducer/step": function(obs, input) {
-			return obs.next(input);
-		},
-		"@@transducer/result": function(obs) {
-			return obs.completed();
-		}
-	};
-}
-
-/**
- * Executes a transducer to transform the observable sequence
- * @param {Transducer} transducer A transducer to execute
- * @returns {Observable} An Observable sequence containing the results from the transducer.
- */
-function transduceObservable(source, xf) {
-	return new Observable(function(o) {
-		return source.subscribe(new TransduceObserver(o, xf(transformForObserver(o))));
-	}, source);
-}
 
 export function LazySeq(iterable){
 	this.iterable = isSeq(iterable) ? iterable.iterable : iterable || [];
@@ -105,19 +64,35 @@ LazySeq.prototype.toString = function(){
 	return "Seq [" + this.iterable + "]";
 };
 
+LazySeq.prototype.toObservable = function(){
+	let iterable = this.iterable;
+	return isObservable(iterable) ? iterable : Observable.from(iterable);
+};
+
+LazySeq.prototype.asObservable = function(){
+	return new LazySeq(this.toObservable());
+};
+
+LazySeq.prototype.toArray = function() {
+	const iterable = this.iterable;
+	if(isObservable(iterable)) return iterable.reduce((a,x) => {
+		a.push(x);
+		return a;
+	},[]);
+	return Array.from(iterable);
+};
+
+LazySeq.prototype.asArray = function(){
+	return new LazySeq(this.toArray());
+};
+
 LazySeq.prototype.transform = function(xf){
-	var iterable = this.iterable;
-	if(_isObservable(iterable)) return new LazySeq(transduceObservable(this.iterable,xf));
 	return new LazySeq(transform(this.iterable,xf));
 };
 
 LazySeq.prototype.count = function(){
 	const iter = this.iterable;
 	return _isArray(iter) ? iter.length : Infinity;
-};
-
-LazySeq.prototype.toArray = function() {
-	return Array.from(this.iterable);
 };
 
 // just resolve a seq of promises, like Promise.all
