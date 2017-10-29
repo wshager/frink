0d05aa1e1233a689fab389e9b4128d61b67aefdf
W.S. Hager <wshager@gmail.com>
2017-10-29 20:08:53 +0100

    seqify ensureDoc + constructors

--- src/doc.js
+++ src/doc.js
@@ -1,47 +1,51 @@
 import * as inode from "./inode";
 
-import { isSeq } from "./seq";
+import { seq, isSeq } from "./seq";
 
 import { x } from "./construct";
 
-export function ensureDoc(node){
-	if(!node) return;
+import { isVNode } from "./access";
+
+export function ensureDoc($node){
+	// FIXME if isVNode(node) use cx on node
 	var cx = this && this.vnode ? this : inode;
-	if(!node.inode) {
-		const type = cx.getType(node);
-		if(type == 9 || type == 11) {
-			let root = cx.first(node);
-			return cx.vnode(root, cx.vnode(node), 1, 0);
-		} else {
-			// create a document-fragment by default!
-			let doc = t.bind(cx)();
-			let root = cx.vnode(node, doc, 1, 0);
-			doc = doc.push([0,root.inode]);
-			return root;
+	return seq($node).concatMap(node => {
+		if (!node.inode) {
+			var type = cx.getType(node);
+			if (type == 9 || type == 11) {
+				var root = cx.first(node);
+				return seq(cx.vnode(root, cx.vnode(node), 1, 0));
+			} else {
+				// create a document-fragment by default!
+				var doc = t.bind(cx)();
+				var _root = cx.vnode(node, doc, 1, 0);
+				return doc.concatMap(function (doc) {
+					doc = doc.push([0, _root.inode]);
+					var next = doc.first();
+					return next ? seq(doc.vnode(next, doc, doc.depth + 1, 0)) : seq();
+				});
+			}
 		}
-	}
-	if(typeof node.inode === "function") {
-		node = node.inode(d.bind(cx)());
-	}
-	return node;
+		if (typeof node.inode === "function") {
+			// NOTE never bind to current node.cx, but purposely allow cross-binding
+			return d.bind(cx)(node).concatMap(node => {
+				let next = node.first();
+				return next ? seq(node.vnode(next,node,node.depth + 1, 0)) : seq();
+			});
+		}
+		return seq(node);
+	});
 }
 
 function _d(type,children){
+	children = seq(children).concatMap(c => isSeq(c) ? c : isVNode(c) ? seq(c) : x(c));
 	var cx = this.vnode ? this : inode;
 	let node = cx.vnode(cx.emptyINode(type,"#document"),null,0);
-	if(children === undefined) {
-		children = [];
-	} else if(isSeq(children)) {
-		children = children.toArray();
-	} else if(children.constructor != Array) {
-		if(!children.__is_VNode) children = x(children);
-		children = [children];
-	}
-	for (let i = 0; i < children.length; i++) {
-		let child = children[i];
-		child = child.inode(node);
-	}
-	return node.finalize();
+	return children.concatMap(child => {
+		return child.inode(node);
+	}).reduce(function (node, child) {
+		return node.modify(child);
+	}, node);
 }
 
 export function d(children) {
