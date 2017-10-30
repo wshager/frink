beaeaa95d470f73e47cc07cf8503323b759ab2a5
W.S. Hager <wshager@gmail.com>
2017-10-30 09:00:17 +0100

    build + update test

--- test/construct.js
+++ test/construct.js
@@ -6,9 +6,5 @@ const n = require("../lib/index");
 	),
 	n.a("y",n.l())
 ]);*/
-var e = n.e("test",n.seq(n.e("a","bla"),n.e("b","bli")));
-n.nextSibling(n.firstChild(e)).subscribe(x => console.log(x+""));
-//var e = n.e("test",[n.a("x","test"),n.x("bla")]);
-//n.ensureDoc(e);
-//n.iter(e,x => console.log(x.toString()));
-//Rx.Observable.from(n.select(e,n.list("*")).iterable).subscribe(x => console.log(x+""));
+var e = n.e("test",n.seq(n.a("a","bla"),n.e("b","bli")));
+n.ensureDoc(e).subscribe(x => console.log(x+""));
