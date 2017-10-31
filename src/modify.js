0fb4e6adcc014a063d26a0c19cbd8dafe8826094
W.S. Hager <wshager@gmail.com>
2017-10-31 08:20:45 +0100

    initial fix to appendChild

--- src/modify.js
+++ src/modify.js
@@ -1,6 +1,12 @@
+import { seq } from "./seq";
+
 import { ensureDoc } from "./doc";
 
-import { firstChild } from './access';
+import { firstChild } from "./access";
+
+import * as inode from "./inode";
+
+const _isDocOrFrag = node => node.type == 9 || node.type == 11;
 
 function _ascend(node,cx){
 	var child;
@@ -10,21 +16,25 @@ function _ascend(node,cx){
 		node = node.set(child.name,child.inode);
 	}
 	// this ensures immutability
-	return node.type == 9 ? firstChild.bind(cx)(node) : node;
+	return _isDocOrFrag(node) ? firstChild.bind(cx)(node) : node;
 }
 
 export function appendChild(node, child) {
-	node = ensureDoc.bind(this)(node);
-	//if(!node || !node.size) return;
-	if(node.type == 9 && node.inode.size > 0) {
-		throw new Error("Document can only contain one child.");
-	}
-	if (typeof child.inode === "function") {
-		child.inode(node);
-	} else {
-		node = node.push(child);
-	}
-	return _ascend(node,this);
+	const cx = this && "vnode" in this ? this : inode;
+	return ensureDoc.bind(cx)(node).concatMap(function (node) {
+		//if(!node || !node.size) return;
+		if (_isDocOrFrag(node) && node.count() > 0) {
+			throw new Error("Document can only contain one child.");
+		}
+		return child.concatMap(function (child) {
+			return typeof child.inode === "function" ? child.inode(node) : seq(child);
+		}).reduce(function (node, child) {
+			return node.modify(child);
+		}, node);
+		//.map(node =>
+		//	(0, _seq.seq)(_ascend(node, cx))
+		//);
+	});
 }
 
 export function insertChildBefore(node,ins){
