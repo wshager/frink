1dfd5392f4c084eeb86e61efa378c96cde37c937
W.S. Hager <wshager@gmail.com>
2017-10-31 09:30:07 +0100

    updated test

--- test/construct.js
+++ test/construct.js
@@ -1,10 +1,11 @@
 const n = require("../lib/index");
 
-/*var e = n.m([
+var e = n.e("test",n.seq(n.a("a","bla"),n.e("b","bli")));
+n.ensureDoc(e).subscribe(x => console.log(x+""));
+var m = n.m([
 	n.a("x",
 		n.l([n.x("bla"),n.x("bli")])
 	),
 	n.a("y",n.l())
-]);*/
-var e = n.e("test",n.seq(n.a("a","bla"),n.e("b","bli")));
-n.ensureDoc(e).subscribe(x => console.log(x+""));
+]);
+n.ensureDoc(m).subscribe(x => console.log(x+""));
