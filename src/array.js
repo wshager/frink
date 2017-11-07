e72426f11b68663cb253e1abb8758cd912c9fba5
W.S. Hager <wshager@gmail.com>
2017-11-08 00:44:50 +0100

    initial fixes array

--- src/array.js
+++ src/array.js
@@ -2,9 +2,13 @@ import * as rrb from "rrb-vector";
 
 import { error } from "./error";
 
-import { seq, first, isSeq, foldLeft as seqFoldLeft, zeroOrOne, exactlyOne } from "./seq";
+import { seq, from, of, first, isSeq, foldLeft as seqFoldLeft, zeroOrOne, exactlyOne } from "./seq";
 
-import * as t from "./transducers";
+import { ensureDoc } from "./doc";
+
+import { list } from "./access";
+
+import { isArray, isUndef } from "./util";
 
 
 var List = rrb.empty.constructor;
@@ -37,44 +41,51 @@ rrb.TreeIterator.prototype["@@transducer/result"] = function(l) {
 	return l;
 };
 
-export function isArray($maybe) {
+function _isList(maybe) {
+	return maybe && maybe.__is_List;
+}
+
+export function isList($maybe) {
 	let maybe = first($maybe);
-	return !!(maybe && maybe.__is_List);
 }
 
 export default function array(...a) {
 	var l = a.length;
 	if(l===0){
-		return rrb.empty;
+		return seq(rrb.empty);
 	}
-	if(l==1 && isSeq(a[0])){
-		return rrb.fromArray(a[0].toArray());
+	if(l==1){
+		const s = a[0];
+		if (isSeq(s)) return s;
+		if (_isList(s)) return of(s);
+		if (isArray(s)) return of(rrb.fromArray(s));
+		// TODO VNode conversion
+		if (list()(s)) return of(s.toArray());
+		return rrb.empty.push(s);
 	}
-	return rrb.fromArray(a);
+	return join(seq(a.map(of)).concatAll().map(a => array(a)));
 }
 
 export function join($a) {
 	if ($a === undefined) return error("XPTY0004");
 	// assume a sequence of vectors
-	return seqFoldLeft($a,rrb.empty,function(pre,cur){
-		var v = first(cur);
-		if(!isArray(v)) return error("XPTY0004","One of the items for array:join is not an array.");
-		return pre.concat(v);
-	});
+	return $a.reduce(function(pre,cur){
+		// TODO force persistent cx
+		if(list(cur)) cur = cur.toArray();
+		if(!_isList(cur)) return error("XPTY0004","One of the items for array:join is not an array.");
+		return pre.concat(cur);
+	},rrb.empty);
 }
 
 function _checked($a, fn, ...args) {
 	if ($a === undefined) return error("XPTY0004");
-	var a = $a;
-	if (isSeq($a)) {
-		if ($a.size > 1) return error("XPTY0004");
-		a = first($a);
-	}
-	if (!isArray(a)) return error("XPTY0004");
-	args.unshift(a);
-	return fn.apply(a, args);
+	return exactlyOne($a).map(a => !_isList(a) ? error("XPTY0004") : fn.bind(a,a).apply(a, args));
 }
 
+// TODO iterator to Observable using transducer protocol
+// at the completion of the operation, create a new list
+// array:transform($input, ... functions) => zeroOrOne()
+// for now:
 export function head($a) {
 	return _checked($a, rrb.get,0);
 }
@@ -88,22 +99,22 @@ export function size($a) {
 }
 
 export function subarray($a,$s,$l) {
-	var s =  first($s) || 1, l = first($l);
-	var sx = s.valueOf() - 1;
-	if(l) l = Math.max(sx + Number(l),0);
-	return _checked($a, rrb.slice, sx, l);
+	return exactlyOne($a).concatMap(a => seq($s).concatMap(s => {
+		s = s.valueOf() - 1;
+		$l = seq($l);
+		return $l.isEmpty().map(test => test ? rrb.slice(a,s) : $l.map(l => rrb.slice(a,s,Math.max(s + Number(l),0))));
+	}));
 }
 
 export function insertBefore($a, $i, $v) {
-	var i = first($i) || 1;
-	var ix = i.valueOf() - 1;
-	// unmarshal Singleton
-	var v = isSeq($v) && $v.size != 1 ? $v : first($v);
-	return _checked($a, function(a){
-		// slice from 0 to i
-		// slice form i to end
-		// concat s1 + v + s2
-		return a.slice(0,ix).push(v).concat(a.slice(ix));
+	return seq($i).concatMap(i => {
+		return _checked($a, function(a){
+			const ix = i - 1;
+			// slice from 0 to i
+			// slice form i to end
+			// concat s1 + v + s2
+			return a.slice(0,ix).push($v).concat(a.slice(ix));
+		});
 	});
 }
 
@@ -116,8 +127,7 @@ export function remove($a,$i) {
 }
 
 export function append($a,$v) {
-	var v = isSeq($v) && $v.size != 1 ? $v : first($v);
-	return _checked($a,rrb.push,v);
+	return _checked($a,rrb.push,$v);
 }
 
 export function reverse($a) {
@@ -127,16 +137,15 @@ export function reverse($a) {
 }
 
 export function flatten($a) {
-	return t.into($a, t.compose(t.cat,t.filter(_ => _ !== undefined)), seq());
+	return $a.concatAll();
 }
 
 export function get($a,$i) {
-	var i = first($i) || 1;
-	var ix = i.valueOf() - 1;
-	return _checked($a,function(a){
-		if(a.size) return a.get(ix);
-		return seq();
-	});
+	if(isUndef($i)) return error("XPST0017");
+	return exactlyOne($i).concatMap(i => _checked($a,function(a){
+		if(a.size) return a.get(i - 1);
+		//return seq();
+	}));
 }
 
 export function forEach($a,$f){
@@ -148,5 +157,7 @@ export function filter($a,$f){
 }
 
 export function foldLeft($a,$z,$f){
+	// TODO use iterator to seq, foldLeft seq into new array
+	//return zeroOrOne($a).concatMap(a => seq())
 	return zeroOrOne($a).concatMap(a => exactlyOne($z).concatMap(z => exactlyOne($f).concatMap(f => t.foldLeft(a,z,f))));
 }
