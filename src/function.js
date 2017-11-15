1df66be66894fc6b313b8e44bf035dc3e390148f
W.S. Hager <wshager@gmail.com>
2017-11-15 13:35:27 +0100

    coverted to rx

--- src/function.js
+++ src/function.js
@@ -1,62 +1,47 @@
-import {
-    string, number, boolean, integer, double, float, decimal, data
-} from "./type";
+import { seq, exactlyOne } from "./seq";
 
-import { seq, isSeq, first } from "./seq";
+import { isUndef } from "./util";
 
-import { isNode } from "./access";
-
-import { forEach, filter } from "./transducers";
-
-import { error } from "./error";
-
-import { glt, ggt } from "./op";
+import * as map from "./map";
 
 const modules = {
-    "http://www.w3.org/2005/xpath-functions":exports
+	"http://www.w3.org/2005/xpath-functions":"./function"
 };
 
-export function module(location) {
-    // conflict?
-    //if (module.uri in modules) return;
-    var module = require(location);
-    modules[module.$uri] = module;
-    return module;
+export function loadModule($moduleUri) {
+	// conflict?
+	//if (module.uri in modules) return;
+	// let js manager handle logic
+	return exactlyOne($moduleUri).concatMap(moduleUri => map.default(require(modules[moduleUri])));
 }
 
 function camelCase(str) {
-    return str.split(/-/g).map((_,i) => i > 0 ? _.charAt(0).toUpperCase() + _.substr(1) : _).join("");
+	return str.split(/-/g).map((_,i) => i > 0 ? _.charAt(0).toUpperCase() + _.substr(1) : _).join("");
 }
 
-export function functionLookup($name,$arity){
-    var qname = first($name);
-    var arity = first($arity);
-    var uri = first(qname.uri).toString();
-    var name = camelCase(first(qname.name).toString().split(/:/).pop());
-    var fn = modules[uri][name+"$"+arity];
-    if(!fn) fn = modules[uri][name+"$"];
-    return !!fn ? fn : seq();
+export function functionLookup($qname,$arity){
+	return exactlyOne($qname).concatMap(qname => {
+		return exactlyOne($arity).concatMap(arity => {
+			var uri = qname.uri.toString();
+			var name = camelCase(qname.name.toString().split(/:/).pop());
+			var fn = modules[uri][name+"$"+arity];
+			if(!fn) fn = modules[uri][name+"$"];
+			return fn ? seq(fn) : seq();
+		});
+	});
 }
 
 export function apply($fn,$a) {
-	var a = first($a);
-	if(!(a instanceof Array)){
-		if(typeof a.toJS != "function") return error("");
-	}
-	return first($fn).apply(this,a.toJS(true));
+	return exactlyOne($fn).concatMap(fn => exactlyOne($a).concatAll().toArray().map(a => fn.apply(this,a)));
 }
 
 // FIXME check if seq + apply data
-export function sort(...args){
-    var l = args.length;
-	var s = args[0];
-	if(!isSeq(s)) return s;
-	var crit = l>1 ? first(a[1]) : function(a,b){
-        var gt = ggt(a,b);
-        var lt = glt(a,b);
-		return gt ? 1 : lt ? -1 : 0;
+export function sort($s,$fn){
+	var crit = function(a,b){
+		const hasComp = typeof a == "object" && "gt" in a;
+		const gt = (a,b) => hasComp ? a.gt(b) : a > b;
+		const lt = (a,b) => hasComp ? a.lt(b) : a < b;
+		return gt(a,b) ? 1 : lt(a,b) ? -1 : 0;
 	};
-	return seq(s.toArray().sort(crit));
+	return seq($s).toArray().concatMap(a => isUndef($fn) ? seq(a.sort(crit)) : seq($fn).concatMap(fn => a.sort(fn)));
 }
-
-export { error };
