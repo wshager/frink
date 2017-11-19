97ffeb4d443f713037e2c2e4008b0e14ba310adc
W.S. Hager <wshager@gmail.com>
2017-11-19 19:48:59 +0100

    added form test

--- test/server.js
+++ test/server.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const http = require("http");
 const fs = require("fs");
 const port = 8000;
 
-var express = require('express');
+var express = require("express");
 var app = express();
 app.use(express.static(__dirname+"/.."));
-
+app.use(express.static("d:/workspace/"));
 app.listen(port, function () {
-  console.log('Web server listening on port '+port);
+	console.log("Web server listening on port "+port);
 });
