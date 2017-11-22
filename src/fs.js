c858fda3c99ed8846923fa632c1f4dbd8aecdefe
W.S. Hager <wshager@gmail.com>
2017-11-22 12:11:50 +0100

    fixed readFile

--- src/fs.js
+++ src/fs.js
@@ -1,21 +1,27 @@
-import isNodeEnv from "./util";
+import { isNodeEnv } from "./util";
 
-export const readFile = (...args) =>
-	isNodeEnv() ?
-		require("fs").readFile.apply(this,args) :
-		(source, cb, encoding = "utf-8") => {
-			var reader = new FileReader();
-			reader.onloadend = evt => {
-				// file is loaded
-				cb(null,evt.target.result);
-			};
-			reader.onerror = err => {
-				cb(err);
-			};
-			reader.readAsText(source, encoding);
+export const readFile = (...args) => {
+	const l = args.length;
+	const source = args[0];
+	const cb = l == 2 ? args[1] : args[2];
+	let options = l == 2 ? "utf-8" : args[1];
+	if(typeof options == "string") options = {encoding:options};
+	if(isNodeEnv) {
+		require("fs").readFile(source,options,cb);
+	} else {
+		var reader = new FileReader();
+		reader.onloadend = evt => {
+			// file is loaded
+			cb(null,evt.target.result);
 		};
+		reader.onerror = err => {
+			cb(err);
+		};
+		reader.readAsText(source, options.encoding);
+	}
+};
 
 export const readdir = (...args) =>
-	isNodeEnv() ?
+	isNodeEnv ?
 		require("fs").readdir.apply(this,args) :
 		() => {};
