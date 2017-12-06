7fdc0fd4762add17dac6a70ce2aadc8c884f4404
W.S. Hager <wshager@gmail.com>
2017-12-06 21:52:42 +0100

    first succesful streamingSelect run, fistbump

--- src/access-streaming.js
+++ src/access-streaming.js
@@ -4,12 +4,15 @@ import { error } from "./error";
 
 import { element, attribute } from "./access";
 
+import "rxjs/add/operator/skipWhile";
 import "rxjs/add/operator/takeWhile";
+
 
 //const _vnodeFromCx = (cx,node) => cx && "vnode" in cx ? cx.vnode : node.cx.vnode;
 
 export function children($node,axis) {
-
+	var contextNode, depth = NaN;
+	// move to first contextNode if isNaN, or the same depth
 	var f = axis && axis.f ?
 		axis.f.__is_NodeTypeTest ?
 			axis.f.__Accessor ?
@@ -19,65 +22,68 @@ export function children($node,axis) {
 		n => n;
 	let start = false, end = false;
 	const test = node => {
-		start = start || (node.type != 17 && node.parent === axis.x && f(node));
+		if(isNaN(depth)) depth = node.depth;
+		let switchContext = false;
+		if(node.depth == depth) {
+			switchContext = true;
+			contextNode = node;
+		}
+		start = start || (node.type != 17 && node.parent === contextNode && f(node));
 		const flagEnd = end;
-		end = end || (node.type == 17 && node.parent === axis.x && f(node.node));
-		return start && !flagEnd;
+		end = end || (node.type == 17 && node.parent === contextNode && f(node.node));
+		return switchContext || (start && !flagEnd);
 	};
-	return $node.takeWhile(node => test(node));
+	return $node.takeWhile(node => test(node)).filter(node => node.depth > depth);
 }
 
-function Axis(g, f, type, contextNode) {
+function Axis(g, f, type) {
 	return {
 		__is_Axis: true,
 		__type: type || 1,
-		x: contextNode,
 		f: f,
 		g: g
 	};
 }
-function child($f, contextNode) {
+function child($f) {
 	const cx = this;
 	return seq($f).map(f => {
-		var axis = Axis(void(0), f, 1, contextNode);
+		var axis = Axis(void(0), f, 1);
 		const stepper = $node => children.bind(cx)($node,axis);
 		axis.g = stepper;
 		return axis;
 	});
 }
 
-function _axify($path, contextNode) {
+function _axify($path) {
 	return seq($path).concatMap(path => {
 		if (!path.__is_Axis) {
 			// process strings (can this be combined?)
 			if (typeof path == "string") {
 				var at = /^@/.test(path);
 				if (at) path = path.substring(1);
-				return at ? attribute(path, contextNode) : child(element(path), contextNode);
+				return at ? attribute(path) : child(element(path));
 			} else if (typeof path == "function") {
-				if (path.__is_NodeTypeTest) return child(path, contextNode);
+				if (path.__is_NodeTypeTest) return child(path);
 				return self(path);
 			} else {
 				return error("XXX", "Unknown axis provided");
 			}
 		}
-		if(!path.x) path.x = contextNode;
 		return seq(path);
 	});
 }
 
 export function selectStreaming($node, ...paths) {
 	var cx = this;
-	$node = seq($node);
+	$node = seq($node).skipWhile(node => node.type != 1);
 	// make sure the root is a valid context
-	return $node.first(node => node.type == 1)
-		.concatMap(contextNode =>
-			seq(paths)
-				.concatMap(path => seq(_axify(path, contextNode)))
-			// execute the axis stepping function,
-			// it should now take care of the testing / transformation function
-				.map(path => $node => path.g.bind(cx)($node))
-			// update the context state
-				.reduce(($node, changeFn) => changeFn($node),$node)
-				.concatAll());
+	//
+	return seq(paths)
+		.concatMap(path => seq(_axify(path)))
+		// execute the axis stepping function,
+		// it should now take care of the testing / transformation function
+		.map(path => $node => path.g.bind(cx)($node))
+		// update the context state
+		.reduce(($node, changeFn) => changeFn($node),$node)
+		.concatAll();
 }
